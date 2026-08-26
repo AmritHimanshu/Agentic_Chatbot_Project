12f0eae1f4cc5baa1686ef7b7265bd4caad91ef6
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2026-08-26 22:06:39 +0530

    Connecting Backend and Frontend

--- frontend.py
+++ frontend.py
@@ -35,7 +35,7 @@ if st.button("Ask Agent!"):
             "allow_search": allow_web_search
         }
 
-        response = requests.post(API_URL, json=payload)
+        response = requests.post(f"{API_URL}/chat", json=payload)
 
         if response.status_code == 200:
             response_data = response.json()
